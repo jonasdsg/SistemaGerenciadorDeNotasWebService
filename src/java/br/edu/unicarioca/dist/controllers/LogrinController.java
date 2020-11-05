a8559534f898c25fe541eac58d7989db8364c64f
Jonas Góes <jonasdossantos1991@gmail.com>
2020-11-04 21:45:48 -0300

    Criação de pacotes e classes

---
+++ src/java/br/edu/unicarioca/dist/controllers/LogrinController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unicarioca.dist.controllers;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author jonas
+ */
+@WebService(serviceName = "Logrin")
+public class LogrinController {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
